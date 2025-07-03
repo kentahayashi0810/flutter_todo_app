5b6814c6362d70a118ead999ec1f38111d79291b
kentahayashi0810 <kenta0810hayashi@gmail.com>
2025-07-03 22:24:09 +0900

    added state to todo items

--- lib/todo_detail_page.dart
+++ lib/todo_detail_page.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/todo_list_page.dart';
 
-class TodoDetailPage extends StatelessWidget {
+class TodoDetailPage extends StatefulWidget {
   final TodoItem todoItem;
 
   const TodoDetailPage({super.key, required this.todoItem});
 
+  State<TodoDetailPage> createState() => _TodoDetailPageState();
+}
+
+class _TodoDetailPageState extends State<TodoDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(todoItem.title)),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(todoItem.description, style: TextStyle(fontSize: 18.0)),
+      appBar: AppBar(title: Text(widget.todoItem.title)),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              widget.todoItem.description,
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {
+              setState(() {
+                widget.todoItem.toggleCompleted();
+              });
+            },
+            child: Text(
+              widget.todoItem.completed
+                  ? 'Mark as Completed'
+                  : 'Mark as not Completed',
+              style: TextStyle(color: Colors.black, fontSize: 16.0),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -1,114 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/todo_detail_page.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<TodoItem> todoItems = [
-      TodoItem(title: 'Buy groceries', description: 'Milk, Bread, Eggs'),
-      TodoItem(title: 'Walk the dog', description: '30 minutes in the park'),
-      TodoItem(
-        title: 'Complete Flutter project',
-        description: 'Finish the to-do app',
-      ),
-      TodoItem(
-        title: 'Read a book',
-        description: 'Start with "Flutter for Beginners"',
-      ),
-      TodoItem(
-        title: 'Call Mom',
-        description: 'Check in and see how she is doing',
-      ),
-      TodoItem(title: 'Prepare dinner', description: 'Cook pasta and salad'),
-      TodoItem(title: 'Exercise', description: '30 minutes of cardio'),
-      TodoItem(
-        title: 'Clean the house',
-        description: 'Vacuum and dust all rooms',
-      ),
-      TodoItem(
-        title: 'Plan weekend trip',
-        description: 'Decide on destination and activities',
-      ),
-      TodoItem(
-        title: 'Organize workspace',
-        description: 'Declutter desk and files',
-      ),
-      TodoItem(
-        title: 'Attend meeting',
-        description: 'Discuss project updates with team',
-      ),
-      TodoItem(
-        title: 'Grocery shopping',
-        description: 'Buy fruits and vegetables',
-      ),
-      TodoItem(title: 'Laundry', description: 'Wash and fold clothes'),
-      TodoItem(
-        title: 'Pay bills',
-        description: 'Electricity and internet bills',
-      ),
-      TodoItem(
-        title: 'Study for exam',
-        description: 'Review notes and practice questions',
-      ),
-      TodoItem(title: 'Watch a movie', description: 'Relax and enjoy a film'),
-      TodoItem(
-        title: 'Plan next week',
-        description: 'Set goals and tasks for the week',
-      ),
-      TodoItem(
-        title: 'Meditate',
-        description: 'Spend 10 minutes in mindfulness',
-      ),
-      TodoItem(
-        title: 'Update resume',
-        description: 'Add recent work experience and skills',
-      ),
-      TodoItem(
-        title: 'Practice coding',
-        description: 'Solve problems on LeetCode',
-      ),
-      TodoItem(
-        title: 'Clean the car',
-        description: 'Wash and vacuum the interior',
-      ),
-      TodoItem(
-        title: 'Buy birthday gift',
-        description: 'Find a present for friend\'s birthday',
-      ),
-      TodoItem(
-        title: 'Attend yoga class',
-        description: 'Join the evening session',
-      ),
-      TodoItem(
-        title: 'Write blog post',
-        description: 'Share insights on Flutter development',
-      ),
-      TodoItem(
-        title: 'Plan family gathering',
-        description: 'Organize a get-together',
-      ),
-    ];
+    Future<List<TodoItem>> loadTodoItems() async {
+      final String jsonString = await rootBundle.loadString(
+        'assets/todos.json',
+      );
+      final List<dynamic> jsonList = json.decode(jsonString);
+      return jsonList
+          .map(
+            (item) => TodoItem(
+              title: item['title'],
+              description: item['description'],
+              completed: item['completed'] ?? false,
+            ),
+          )
+          .toList();
+    }
 
     return Scaffold(
       appBar: AppBar(title: const Text('To-do List')),
-      body: Center(
-        child: ListView.builder(
-          itemCount: todoItems.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(todoItems[index].title),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      TodoDetailPage(todoItem: todoItems[index]),
-                ),
-              );
-            },
-          ),
-        ),
+      body: FutureBuilder(
+        future: loadTodoItems(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No to-do items found.'));
+          } else {
+            final List<TodoItem> todoItems = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: todoItems.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(todoItems[index].title),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          TodoDetailPage(todoItem: todoItems[index]),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
       ),
     );
   }
@@ -117,11 +63,15 @@ class TodoListPage extends StatelessWidget {
 class TodoItem {
   final String title;
   final String description;
-  final bool completed;
+  bool completed;
 
   TodoItem({
     required this.title,
     required this.description,
     this.completed = false,
   });
+
+  void toggleCompleted() {
+    completed = !completed;
+  }
 }
